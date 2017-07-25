d2a01477412ecb36dde8661905aa2694c33f234d
vigil varghese <vigil@pc-184.acfr.usyd.edu.au>
2017-07-25 12:19:40 +1000

    initial

--- ConvertLFDataType.m
+++ ConvertLFDataType.m
@@ -1,8 +1,25 @@
 % This function converts between the various types of LF structure to help operating between multiple scripts
-function [A, B, C, D, E] = ConvertLFDataType(spatial_x, spatial_y, angular_x, angular_y, color, hasWeight, outAtype outBtype outCtype outDtype outEtype)
+function LFout = ConvertLFDataType(LFin, OutputFormat)
+  % usage: LFout = ConvertLFDataType(LFin, ['Ax', 'Ay', 'Sx', 'Sy', 'C'])
+  % auto detects input format
+  % LFin        -> Light field to be converted
+  % LFout       -> Converted light field
+  % InputFormat -> list([]) with input type in order - one of - spatial_x -> 'Sx'
+  %                                                             spatial_y -> 'Sy'
+  %                                                             angular_x -> 'Ax'
+  %                                                             angular_y -> 'Ay'
+  %                                                             color (no weight) -> 'C'
+  %                                                             color (with weight) -> 'CW'
+  % OutputFormat -> list([]) with output type in order - one of - spatial_x -> 'Sx'
+  %                                                               spatial_y -> 'Sy'
+  %                                                               angular_x -> 'Ax'
+  %                                                               angular_y -> 'Ay'
+  %                                                               color (no weight) -> 'C'
+  %                                                               color (with weight) -> 'CW' -> only works if input had CW
+
   % spatial_x   -> spatial (image) x resolution
   % spatial_y   -> spatial (image) y resolution
   % angular_x   -> angular (sub-pixel) x resolution
   % angular_y   -> angular (sub-pixel) y resolution
-
-  
+  % color (C)   -> color channel (rgb - 1:3)
+  % CW          -> color channel also has a weight parameter from LightFieldToolbox0.4 (rgbw - 1:4)
